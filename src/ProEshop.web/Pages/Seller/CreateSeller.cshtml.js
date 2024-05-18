bf31e47513eef4c5bd7211fe708d19297137adf0
Ali Akbar Ebrahimi <a.ebrahimi1367@gmail.com>
2024-05-18 13:28:59 +0330

    072_Implement Next and Previous buttons in Create Seller Page

--- src/ProEshop.web/Pages/Seller/CreateSeller.cshtml.js
+++ src/ProEshop.web/Pages/Seller/CreateSeller.cshtml.js
@@ -1,7 +1,7 @@
 ﻿$('#legal-person-checkbox-create-seller').change(function () {
     var labelElement = $(this).parents('.form-switch').find('label');
     if (this.checked) {
-        addRequiredRule('#CCreateSeller_CompanyName');
+        addRequiredRule('#CreateSeller_CompanyName');
         addRequiredRule('#CreateSeller_RegisterNumber');
         addRequiredRule('#CreateSeller_EconomicCode');
         addRequiredRule('#CreateSeller_SignatureOwners');
@@ -9,7 +9,7 @@
         labelElement.html('شخص حقوقی');
     }
     else {
-        removeRequiredRule('#CCreateSeller_CompanyName');
+        removeRequiredRule('#CreateSeller_CompanyName');
         removeRequiredRule('#CreateSeller_RegisterNumber');
         removeRequiredRule('#CreateSeller_EconomicCode');
         removeRequiredRule('#CreateSeller_SignatureOwners');
@@ -24,7 +24,8 @@
 $('#legal-person-box-create-seller').hide(0);
 
 function addRequiredRule(selector) {
-    var displayName = $(selector).parent().find('label').html().trim();
+    var labelElement = $(selector).parent().find('label');
+    var displayName = labelElement.length > 0 ? labelElement.html().trim() : $(selector).attr('name');
     $(selector).rules('add', {
         required: true,
         messages: {
@@ -33,6 +34,47 @@ function addRequiredRule(selector) {
     });
 }
 
+
 function removeRequiredRule(selector) {
+    var labelElement = $(selector).parent().find('label');
+    var displayName = labelElement.length > 0 ? labelElement.html().trim() : $(selector).attr('name');
     $(selector).rules('remove', 'required');
 }
+
+var firstTab = $('#create-seller-container .nav-tabs button:first').attr('data-bs-target');
+var lastTab = $('#create-seller-container .nav-tabs button:last').attr('data-bs-target');
+$('#create-seller-container #previous-tab-create-seller').attr('disabled', 'disabled');
+
+var currentTab = $('#create-seller-container .nav-tabs button:first').attr('data-bs-target');
+
+$('#create-seller-container #next-tab-create-seller').click(function () {
+    var nextTab = $(`#create-seller-container .nav-tabs button[data-bs-target="${currentTab}"]`).next();
+    if (nextTab.attr('data-bs-target')) {
+        currentTab = nextTab.attr('data-bs-target');
+        nextTab.tab('show');
+    }
+});
+$('#create-seller-container #previous-tab-create-seller').click(function () {
+    var previousTab = $(`#create-seller-container .nav-tabs button[data-bs-target="${currentTab}"]`).prev();
+    if (previousTab.attr('data-bs-target')) {
+        currentTab = previousTab.attr('data-bs-target');
+        previousTab.tab('show');
+    }
+});
+
+$('#create-seller-container .nav-tabs button').on('show.bs.tab', function (e) {
+    currentTab = $(e.target).attr('data-bs-target');
+    if (currentTab == lastTab) {
+        $('#create-seller-container #next-tab-create-seller').attr('disabled', 'disabled');
+    }
+    else {
+        $('#create-seller-container #next-tab-create-seller').removeAttr('disabled');
+    }
+    if (currentTab == firstTab) {
+        $('#create-seller-container #previous-tab-create-seller').attr('disabled', 'disabled');
+    }
+    else {
+        $('#create-seller-container #previous-tab-create-seller').removeAttr('disabled');
+    }
+
+});
